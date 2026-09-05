03681149e1733a4625d10039a8e66b55f5ed614e
Massimo <maxpucci78@gmail.com>
2026-09-05 23:36:48 +0200

    modified the date and time to local zone info

--- ListCommand.py
+++ ListCommand.py
@@ -1,4 +1,5 @@
 import base64
+import datetime as dt
 import email
 from Command import Command
 from email import policy, utils
@@ -19,13 +20,16 @@ class ListCommand(Command):
             msg_date = mime_msg["date"]
 
             index += 1
+            utc_date = dt.datetime.strptime(msg_date,"%a, %d %b %Y %H:%M:%S %z")
+            date_now = utc_date.astimezone()
+            date_str = f"{date_now.day:02}/{date_now.month:02}/{date_now.year:4} {date_now.hour:02}:{date_now.minute:02}"
             if "UNREAD" in msg_detail["labelIds"]:
                 green_color = "\033[32m"
                 reset_color = "\033[0m"
-                print(f"{green_color}Message {index:2}: Date {msg_date[:26]} From {msg_from}")
+                print(f"{green_color}Message {index:2}: Date {date_str} From {msg_from}")
                 print(f"            Subject: {msg_subject[:150]}{reset_color}")
             else:
-                print(f"Message {index:2}: Date {msg_date[:26]} From {msg_from}")
+                print(f"Message {index:2}: Date {date_str} From {msg_from}")
                 print(f"            Subject: {msg_subject[:150]}")
 
 
